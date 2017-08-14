5f8e92c2b1d69b3287f858e2de33594f6b643b35
Mikael Biström <info@studiotriangular.se>
2017-08-14 22:42:41 +0200

    Adding mime_type and sizehandle to migration

--- src/Migrations/2017_08_04_000000_create_images_table.php
+++ src/Migrations/2017_08_04_000000_create_images_table.php
@@ -21,7 +21,9 @@ class CreateImagesTable extends Migration
             $table->string('description');
             $table->string('alt_text');
             $table->string('path');
+            $table->string('mime_type');
             $table->string('size');
+            $table->string('size_handle');
             $table->timestamps();
         });
     }
